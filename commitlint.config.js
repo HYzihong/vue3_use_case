6a1ce5440b7776028a72958ac7d4cfd2010680e2
hy <zihong2020@foxmail.com>
2022-02-01 03:31:53 +0800

    ci(all): init Devops

---
+++ commitlint.config.js
@@ -0,0 +1,13 @@
+/**
+ * @Author: hy
+ * @Date: 2022-02-01 03:12:42
+ * @LastEditors: hy
+ * @Description:
+ * @LastEditTime: 2022-02-01 03:12:42
+ * @FilePath: /vue3UseCase/commitlint.config.js
+ * @Copyright 2022 hy, All Rights Reserved.
+ * @仅供学习使用~
+ **/
+module.exports = {
+  extends: ["@commitlint/config-conventional"],
+};
---
+++ stylelint.config.js
@@ -0,0 +1,13 @@
+/**
+ * @Author: hy
+ * @Date: 2022-02-01 03:18:45
+ * @LastEditors: hy
+ * @Description:
+ * @LastEditTime: 2022-02-01 03:18:46
+ * @FilePath: /vue3UseCase/stylelint.config.js
+ * @Copyright 2022 hy, All Rights Reserved.
+ * @仅供学习使用~
+ **/
+module.exports = {
+  extends: ["stylelint-config-standard", "tylelint-config-standard-scss"],
+};
