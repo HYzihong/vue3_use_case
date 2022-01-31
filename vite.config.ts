b8e7a0cb0e63eee5d913fe3fd62c073d5318f21c
hy <zihong2020@foxmail.com>
2022-02-01 01:33:15 +0800

    add style

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,47 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import { resolve } from "path";
+// plugins
+import vue from "@vitejs/plugin-vue";
+import WindiCSS from "vite-plugin-windicss";
+import jsx from "@vitejs/plugin-vue-jsx";
 
-// https://vitejs.dev/config/
+function pathResolve(dir: string) {
+  // console.log(resolve(process.cwd(), ".", dir));
+  return resolve(process.cwd(), ".", dir);
+}
+
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue(), WindiCSS(), jsx()],
+  resolve: {
+    alias: [
+      {
+        find: /\@\//,
+        replacement: pathResolve("src") + "/",
+      },
+      {
+        find: /\@views\//,
+        replacement: pathResolve("src/views") + "/",
+      },
+      {
+        find: /\@types\//,
+        replacement: pathResolve("src/types") + "/",
+      },
+      {
+        find: /\@comps\//,
+        replacement: pathResolve("src/components") + "/",
+      },
+      {
+        find: /\@utils\//,
+        replacement: pathResolve("src/utils") + "/",
+      },
+    ],
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "./src/styles/index.scss";`,
+        charset: false,
+      },
+    },
+  },
+});
