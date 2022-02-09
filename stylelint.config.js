95b15475e32936b762b337d80c4d9eff603cf4df
hy <zihong2020@foxmail.com>
2022-02-09 23:23:01 +0800

    fix(config): fix code config

--- stylelint.config.js
+++ stylelint.config.js
@@ -3,7 +3,7 @@
  * @Date: 2022-02-01 03:18:45
  * @LastEditors: hy
  * @Description:
- * @LastEditTime: 2022-02-08 02:26:55
+ * @LastEditTime: 2022-02-09 22:25:47
  * @FilePath: /vue3UseCase/stylelint.config.js
  * @Copyright 2022 hy, All Rights Reserved.
  * @仅供学习使用~
@@ -20,6 +20,7 @@ module.exports = {
   customSyntax: 'postcss-html',
   rules: {
     'string-quotes': 'single', // 单引号
+    // 'rule-empty-line-before': 'always',
     //   'at-rule-no-unknown': [
     //     true,
     //     {
