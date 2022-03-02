8400d9d700100592b869833e5568b11cd9c8af46
hy <zihong2020@foxmail.com>
2022-03-02 22:39:23 +0800

    ci(config): add commit message cmd help
    add chinese help

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,11 +3,11 @@
  * @Date: 2022-02-01 03:12:42
  * @LastEditors: hy
  * @Description:
- * @LastEditTime: 2022-02-01 03:12:42
+ * @LastEditTime: 2022-03-02 22:29:08
  * @FilePath: /vue3UseCase/commitlint.config.js
  * @Copyright 2022 hy, All Rights Reserved.
  * @仅供学习使用~
  **/
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
 };
